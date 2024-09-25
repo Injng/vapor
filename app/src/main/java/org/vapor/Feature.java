0f0c8b5c3c892934ed7992739ee09f0a9111d303
Lin Jiang <lnjng07@protonmail.com>
2024-09-24 18:00:09 -0700

    remove useless autocorrelation

--- app/src/main/java/org/vapor/Feature.java
+++ app/src/main/java/org/vapor/Feature.java
@@ -21,15 +21,6 @@ public class Feature {
     /** The y-coordinate of the feature in the image. */
     public int y;
 
-    /** The cumulative sum of the pixel intensities in a 11x11 window around the feature. */
-    public int a;
-
-    /** The cumulative sum of the pixel intensities squared in a 11x11 window around the feature. */
-    public int b;
-
-    /** A value inversely proportional to the standard deviation of the pixel intensities in a 11x11 window around the feature. */
-    public double c;
-
     private int hashCode;
 
     /**
@@ -44,25 +35,9 @@ public class Feature {
         if (x < 5 || x >= image.length - 5 || y < 5 || y >= image[0].length - 5) {
             throw new IllegalArgumentException("Feature coordinates must be at least 5 pixels away from the border.");
         }
-        
-        // set and compute values
-        this.value = value;
-        this.x = x;
-        this.y = y;
-        this.a = 0;
-        this.b = 0;
-        this.c = 0;
-        for (int i = -5; i <= 5; i++) {
-            for (int j = -5; j <= 5; j++) {
-                int pixel = image[x + i][y + j];
-                this.a += pixel;
-                this.b += pixel * pixel;
-            }
-        }
-        this.c = 1 / Math.sqrt((121 * this.b - this.a * this.a));
 
         // set hash code
-        this.hashCode = Objects.hash(value, x, y, image);
+        this.hashCode = Objects.hash(value, x, y);
     }
 
     /**
